e2b7ce863ff915156af9d12d8c2adc24af73a225
Yotam7298 <yoyo729800@gmail.com>
2023-01-03 00:04:38 +0200

    added errors and user controllers

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const port = 3000;
 const mongoUrl = 'mongodb://localhost:27017/cookbookdb';
+const jwtKey = 'secret-key';
 
-module.exports = { port, mongoUrl };
+module.exports = { port, mongoUrl, jwtKey };
